ad2bc8c568a44d1488e02e7ad431bd8326e45a4c
Sebastian Senge <mail@sebastian-senge.de>
2021-08-16 22:01:01 +0200

    Added requirements.txt, updated README and fixed some imports in Config.py

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # Qroestl
 A thin optimization layer on top of Qiskit.
+
+## Install
+
+First clone the repo:
+```bash
+$ cgit clone https://github.com/ssenge/Qroestl.git
+```
+
+Then create an environment, e.g. using _conda_:
+```bash
+$ conda create -n qc  python=3.8
+```
+
+(Note that it seems as the latest version of CPLEX does not work with Python 3.9, so use 3.8 if you want to use CPLEX.)
+
+Activate the environment:
+
+```bash
+$ conda activate qc
+```
+
+Install requirements:
+```bash
+$ pip install -r requirements.txt
+```
+
+Optionally, if you want to use CPLEX install it according to the official [documentation](https://www.ibm.com/products/ilog-cplex-optimization-studio).
+
+## Setup
+
+If you want to use IBM Q / IonQ / DWave cloud services, sign up on the respective web page and follow the instructions to get the required _api token_.
+
+## Config
+
+Open [conf/Config.py](conf/Config.py) and setup the problem to solve. Finally, run:
+
+```bash
+$ python Main.py
+```
+
+
+
--- conf/Config.py
+++ conf/Config.py
@@ -1,14 +1,16 @@
 from qiskit import BasicAer
 from qiskit.algorithms.optimizers import COBYLA
 from qiskit.utils import QuantumInstance
+from qiskit_ionq import IonQProvider
+from qiskit import IBMQ
 
 from model import Model
 from backends import Qiskit
-from problems import ExactCover
+from problems import ExactCover, SetCover
 
 
 class Config:
-    # IonQ_token = 'put-your-token-here'
+    IonQ_token = 'put-your-token-here'
     # IBMQ token: see utils.SaveIBMQToken.py
 
     qdev = (
@@ -21,10 +23,9 @@ class Config:
     p = (
         # SetCover.Generator.linear(20, 5)
         # SetCover.Problem([1, 2, 3, 4, 5, 6, 7], [[7, 1], [1, 2, 3], [4, 5], [6]])
-        # SetCover.Problem([1, 2], [[1], [2]])
-        # SetCover.Problem([1, 2], [[1], [2]])
+        SetCover.Problem([1, 2], [[1], [2]])
 
-        ExactCover.Problem([1, 2, 3, 4, 5, 6, 7], [[7], [1, 2, 3], [4, 5], [6]])
+        # ExactCover.Problem([1, 2, 3, 4, 5, 6, 7], [[7], [1, 2, 3], [4, 5], [6]])
 
         # MaxSetCoverageProblem(k=2, S=[[7], [1, 2], [3]])  # VQE: 152sec
         # SetCoverage.Problem(k=2, S=[[7], [1, 2]])
@@ -32,8 +33,8 @@ class Config:
 
     # Problem-specific algorithms
     sas = [
-        # SetCover.Greedy()
-        ExactCover.Greedy()
+        SetCover.Greedy()
+        # ExactCover.Greedy()
         # MaxSetCoverage.Greedy()
     ]
 
