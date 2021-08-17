97727744613a8ee3548a0371870b37a3afb521d9
Sebastian Senge <mail@sebastian-senge.de>
2021-08-17 18:35:41 +0200

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="Qroestl",
-    version="0.1",
+    version="0.1.1",
     author="Sebastian Senge",
     author_email="ssenge.public@gmail.com",
     description="A thin optimization layer on top of Qiskit.",
