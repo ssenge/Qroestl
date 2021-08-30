4fdff96bf8b548a0d48fc110d7e0c9a071c04a85
egness <egness@egness.info>
2021-08-30 20:18:23 +0200

    Fixed a bug in case of empty sets

--- qroestl/utils/Utils.py
+++ qroestl/utils/Utils.py
@@ -13,7 +13,7 @@ import qiskit
 id = lambda x: x
 where = lambda s, elem: np.nonzero(np.vectorize(lambda e: e == elem)(s))[0][0]
 powerset = lambda s: map(list, chain.from_iterable(combinations(s, r) for r in range(1, len(s)+1)))
-union = lambda s, subset_idx = None: reduce(np.union1d, np.take(s, subset_idx if subset_idx else range(len(s)), axis=0))
+union = lambda s, subset_idx = None: reduce(np.union1d, np.take(s, subset_idx if subset_idx else range(len(s)), axis=0)) if len(s) > 0 else []
 union_non_sorted = lambda s: np.array(reduce(lambda a, b: a+b, s)) if len(s) > 0 else []
 bits2idx = lambda n: lambda bits: [i for i, x in enumerate(bits[:n]) if x == 1]
 
